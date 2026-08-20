3ce711b82a5994712ef6457c5cb4859a0319266e
Vinvic21 <victorkipngeno877@gmail.com>
2026-08-20 14:42:00 +0300

    implemented continuous intergration

--- app.py
+++ app.py
@@ -5,6 +5,11 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return jsonify({"message": "Welcome to Farmrt"})
+@app.route("/status")
+
+def status():
+    return jsonify(status="ok"),200
+
 
 if __name__ == '__main__':
     app.run(debug=True)

7c40e2e9f9467c5d87242c3ac2c5f7257da14b36
Vinvic21 <victorkipngeno877@gmail.com>
2026-08-21 22:11:38 +0300

    Add cart/cart_item models, initial migration, requirements updates

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask, jsonify
+from extensions import db, ma
+from flask_migrate import Migrate
+from models import User, Profile, Animal, Cart, CartItem
+import os
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL","sqlite:///farmart.db").replace("postgres://", "postgresql://", 1)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db.init_app(app)
+ma.init_app(app)
+
+migrate = Migrate(app, db)
 
 @app.route('/')
 def home():
